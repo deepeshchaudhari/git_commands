d424d3a98038e47c37d83d87f29ca24aeb1c2790
Deepesh Chaudhari <deepeshchaudhari0786@gmail.com>
2021-01-28 11:17:34 +0530

    RSA key steup
    RSA key setup

--- commands.sh
+++ commands.sh
@@ -1,2 +1,8 @@
 #check which process uses how much memory
 ps -o pid,user,%mem,command ax |sort -b -k3 -r | head -10
+
+# RSA key setup
+# for generating private and public key
+ssh-keygen -t rsa
+#  for copy public key to server
+ssh-copy-id <ip>
