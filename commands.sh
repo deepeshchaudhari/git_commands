39ba9083e12371f758510b9c7fe7138b9afad68e
Deepesh Chaudhari <deepeshchaudhari0786@gmail.com>
2021-01-28 11:32:55 +0530

    chmod command explained
    chmod command explained

--- commands.sh
+++ commands.sh
@@ -6,3 +6,14 @@ ps -o pid,user,%mem,command ax |sort -b -k3 -r | head -10
 ssh-keygen -t rsa
 #  for copy public key to server
 ssh-copy-id <ip>
+# CHMOD Command
+# 10 char string [owner group anyother]
+# read(r) write(w) execute(x) no permission(-)
+chmod +744
+# command give_permission  owner  group  anyother
+#                           rwx    rwx     rwx
+# chmod         +           111    100     100
+
+
+
+
